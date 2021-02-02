b9010d7453866f628705ef1defe2d8e5732771a0
minmeeen <76035644+minmeeen@users.noreply.github.com>
2021-02-02 16:10:54 +0700

    Update lab14_4.cpp

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,19 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a, int &b, int &c, int &d){
+	int array[4] = {a,b,c,d};
+	int w,x,y,z;
+	do{
+		w= array[rand() % 4];
+		x= array[rand() % 4];
+		y=array[rand() % 4];
+		z=array[rand() % 4];
+		
+	}while(w==x || w==y || w==z || x==y || x==z || y==z);
+	a=w;
+	b=x;
+	c=y;
+	d=z;
+}
