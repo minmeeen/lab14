4d4ffc8d565d6886880b99631110ca3a276e9397
minmeeen <76035644+minmeeen@users.noreply.github.com>
2021-02-02 16:12:20 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,27 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a =5;
+	char b ='A';
+	char &c = b;
+	int *x ;
+	x = &a;
+	char *y ;
+	y = &b;
+	int **z;
+	z = &x;
+	
+	cout << a <<" "<< b << " "<<c<<" "<< x <<" " << (void*)y << " " <<z ;
+	cout << "\n" << &a << " " << (void*)&b << " " << (void*)&c << " " << &x << " " << &y << " " << &z;
+	c= 'F';
+	cout << "\n"<<a <<" "<< b << " "<< c << " "<< x <<" " << (void*)y << " " <<z ;
+	*y = 'W';
+	cout <<"\n"<< a <<" "<< b << " "<< c << " "<<x <<" " << (void*)y<< " " <<z ;
+	*x = 6;
+	cout <<"\n"<< a <<" "<< b << " "<< c << " "<< x <<" " << (void*)y<< " " <<z ;
+	**z=7;
+	cout <<"\n"<< a <<" "<< b << " "<< c << " "<< x <<" " << (void*)y << " " <<z;
 
-	
-	
 	return 0;
 }
